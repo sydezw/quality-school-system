0c3bb865e9deb9e44249b4ca36ca52ce0f5eb217
sydezw <kauanbrunodasilva@gmail.com>
2025-07-11 17:48:39 -0300

    fix: resolver problema de dependências do rollup

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-  ],
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-  optimizeDeps: {
-    include: [
-      "@radix-ui/react-checkbox",
-      "@radix-ui/react-slot",
-      "lucide-react"
-    ]
-  },
   build: {
-    commonjsOptions: {
-      include: [/node_modules/]
-    }
-  }
-}));
+    rollupOptions: {
+      external: [],
+    },
+  },
+})
